3fdcf44cfb2947cb52c50f02e94cc084e3c8ad4c
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2023-10-24 10:19:44 +0400

    Create reversestring.s

---
+++ reversestring.s
@@ -0,0 +1,58 @@
+.data
+str: .asciiz "Ansh"
+msg1: .asciiz "Reversed String : "
+new_line: .asciiz "\n"
+rev_str: .space 256			#allocating 256 character of memory for the string 
+
+.text
+.globl main       
+main:
+    la $t4, str                         # get str address
+                                        # start loop at i = 0
+    li $t2, 0                           # set loop counter
+
+len_loop:
+    lb $t1, 0($t4)                      # load str[i]
+    beqz $t1, len_loop_end              # check for str end
+
+    addi $t2, $t2, 1                    # advance loop counter
+    addi $t4, $t4, 1                    # advance str ptr
+
+    j len_loop                          # repeat the loop
+
+len_loop_end:
+    addi $t4, $t4, -1                   # reduce str ptr
+
+    la $t5, rev_str                     # get rev str address
+
+                                        # start loop at j = 0
+    li $t0, 0                           # set loop counter
+
+rev_loop:
+    beq $t0, $t2, rev_loop_end          # iterate for t2 iters
+
+    lb $t1, 0($t4)                      # load t1 = str[i]
+    sb $t1, 0($t5)                      # store rev_str[j] = t1
+
+    addi $t0, $t0, 1                    # advance loop counter
+    addi $t4, $t4, -1                   # reduce str ptr
+    addi $t5, $t5, 1                    # advance rev_str ptr
+
+    j rev_loop                          # repeat the loop
+
+rev_loop_end:
+    li $v0, 4
+    la $a0, msg1
+    syscall
+
+    li $v0, 4
+    la $a0, rev_str
+    syscall
+
+    li $v0, 4
+    la $a0, new_line
+    syscall
+
+    li $v0,10
+    syscall
+    .end main
